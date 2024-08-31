eb33dffed4c3b06d399a4bf2ed998d76c286c3aa
JoaoGSDC <joaogsdc@gmail.com>
2024-08-31 02:00:48 -0300

    feat: villa marques 2

---
+++ src/app/villamarques2/client.ts
@@ -0,0 +1,30 @@
+export const villamarques = {
+  name: "Villa Marques",
+  pathname: "villamarques",
+  font: `'Montserrat', sans-serif`,
+  logo: "https://ccmpedidoonline.com.br/cdn/logovilamarquescasadelanches.png",
+  color: "#ffffff",
+  backgroundColor: "#1B341E",
+  backgroundImage:
+    "https://res.cloudinary.com/dmwgzmzr9/image/upload/v1725080379/bylink/villa-marques-background-v2_uext2p.png",
+  buttonBackgroundColor: "#D4AF37",
+  buttonColor: "#1B341E",
+  links: [
+    {
+      id: 1,
+      title: "Faça seu pedido",
+      url: "https://vilamarquescasadelanches.ccmpedidoonline.com.br/",
+    },
+    {
+      id: 2,
+      title: "Instagram",
+      url: "https://www.instagram.com/villamarquess/",
+    },
+    { id: 3, title: "Facebook", url: "https://www.facebook.com/villamarquess" },
+    {
+      id: 4,
+      title: "Localização",
+      url: "https://maps.app.goo.gl/EjpnBCwcfXU1SLUs6",
+    },
+  ],
+};
